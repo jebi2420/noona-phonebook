155a2de6c77bb234be712bcc4bb71136308ef7bc
jebi2420 <jebi2420@gmail.com>
2024-04-01 00:34:03 +0900

    [add] 연락처 추가

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,8 +1,24 @@
-let initialState = {};
+let initialState = {
+    contactList:[]
+};
 
 // state는 항상 initialState가 있어야 한다.
 function reducer(state=initialState, action){
+  const {type,payload} = action // destructing: 객체인 action을 분해해서 속성값 각각 쓰는 것
 
+  switch(type){
+    case "ADD_CONTACT":
+      return{...state, contactList:
+        [...state.contactList,
+          { 
+              name: payload.name, 
+              phoneNumber: payload.phoneNumber
+          }
+        ]
+      }
+    default:   
+      return {...state};
+  }
 }
 
 export default reducer;
